fabbd16e55308e500557a75a7cf7be8e7252ad92
Curtis Wendel <cwendel16834@gmail.com>
2013-02-05 13:32:32 -0600

    added Record.cpp and Table.cpp, compiles

---
+++ Record.cpp
@@ -0,0 +1,12 @@
+#include "Record.h"
+
+Record::Record(int numEntries) {
+
+}
+
+int Record::updateEntry(int entryNum, string data) {
+	return 0;
+}
+string Record::getEntry(int entryNum) {
+	return 0;
+}
---
+++ Table.cpp
@@ -0,0 +1,43 @@
+#include "Table.h"
+
+Table::Table() {
+
+}
+Table::Table(Entry entries[]) {
+
+}
+int Table::addEntry(Entry newEntry) {
+	return 0;
+}
+int Table::deleteEntry(Entry oldEntry) {
+	return 0;
+}
+int Table::insertRecord(Record newRecord) {
+	return 0;
+}
+string Table::getAttributes() {
+	return "";
+}
+int Table::getSize() {
+	return 0;
+}
+int Table::renameAttribute(string oldName, string newName) {
+	return 0;
+}
+Table& Table::join(Table& table) {
+	return *this;
+}
+double Table::sum(string attrName) {
+	return 0;
+}
+int Table::count(string attrName) {
+	return 0;
+}
+Record& Table::min(string attrName) {
+	Record record(0);
+	return record;
+}
+Record& Table::max(string attrName) {
+	Record record(0);
+	return record;
+}
