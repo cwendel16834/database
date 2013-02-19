9f1358a2b71eb2b768c104134d4274ffd1ce2f7e
Curtis Wendel <cwendel16834@gmail.com>
2013-02-19 15:38:08 -0600

    added file for iterator implementation

---
+++ TableIterator.cpp
@@ -0,0 +1,31 @@
+#include "Table.h"
+
+typedef Table::TableIterator TableIterator;
+
+/*TableIterator::TableIterator() {
+	
+}
+
+TableIterator::TableIterator(const TableIterator& it) {
+	
+}
+
+TableIterator& TableIterator::operator++() {
+
+}
+
+TableIterator& TableIterator::operator++(int) {
+	
+}
+
+bool TableIterator::operator==(const TableIterator& rhs) {
+	
+}
+
+bool TableIterator::operator!=(const TableIterator& rhs) {
+	
+}
+
+Record& TableIterator::operator*() {
+	
+}*/
