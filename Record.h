6e3456ae7ace11dcbadbcb27900754a8f2ff8362
RYAN WILLIAM COVERICK <rcoverick@gmail.com>
2013-02-21 23:08:21 -0600

    formatting record

--- Record.h
+++ Record.h
@@ -17,23 +17,20 @@ class Table;
 class DATABASE_API Record {
 
 public:
-	Record(); //Creates a Record object with no values
-	Record(int numValues); //Creates a Record object with numValues values
-	~Record(); 
-	int updateValue(int valueNum, string value) throw(invalid_argument) ; //Updates value at position valueNum to value given in data
-	string getValue(int valueNum) throw(invalid_argument); //Gets the value data at position valueNum
-	void addValue(string value = "NULL"); //Adds another space for a value value at the end of the Record
-	int getNumValues(); //Gets the number of values in this Record
+ Record(); 
+ Record(int numValues); //Creates a Record object with numValues values
+ ~Record(); 
+ int updateValue(int valueNum, string value) throw(invalid_argument) ; //Updates value at position valueNum to value given in data
+ string getValue(int valueNum) throw(invalid_argument); //Gets the value data at position valueNum
+ void addValue(string value = "NULL"); //Adds another space for a value value at the end of the Record
+ int getNumValues(); //Gets the number of values in this Record
 
-	static Record joinRecords(Record& rec1, Record& rec2);
-
-	friend class Table;
-
+ static Record joinRecords(Record& rec1, Record& rec2);
+ friend class Table;
 private:
-	vector<string> values;
-	int key;
-
-	static int currentKey;
+ vector<string> values;
+ int key;
+ static int currentKey;
 };
 
 #endif
